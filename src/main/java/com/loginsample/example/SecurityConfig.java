9bc6aac776567176073487c69ba8c7aac50e87f2
ymzkjpx <ymzkjpx@gmail.com>
2021-11-19 14:56:53 +0900

    Logout機能を実装

--- src/main/java/com/loginsample/example/SecurityConfig.java
+++ src/main/java/com/loginsample/example/SecurityConfig.java
@@ -8,11 +8,21 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    private final LogoutHandler logoutHandler;
+
+    public SecurityConfig(LogoutHandler logoutHandler) {
+        this.logoutHandler = logoutHandler;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.authorizeRequests()
-               .mvcMatchers("/", "/images/**").permitAll()
-               .anyRequest().authenticated()
-           .and().oauth2Login();
+        http.authorizeRequests()
+                .mvcMatchers("/", "/images/**").permitAll()
+                .anyRequest().authenticated()
+                .and().oauth2Login()
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .addLogoutHandler(logoutHandler);
     }
 }
